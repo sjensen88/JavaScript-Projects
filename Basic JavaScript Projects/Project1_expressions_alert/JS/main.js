6ff71698977c0494661a7291e255314cb42acf0c
Stephanie <sjensen88@gmail.com>
2024-01-26 17:17:28 -0600

    changed repo structure

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var greeting = ("Hello, TTA instructors! Welcome to my JavaScript assignment submission!");
+window.alert(greeting);
+
+var sent1 = ("This is the beginning of the string");        //assigned two variables (sent1 and sent2) string values
+var sent2 = (" and this is the end of the string.");
+
+document.write(sent1 + sent2);
+
+document.write("\"This is a string quote"
+    + " that has been concatenated.\"");                  //the + operator is used to concatenate strings in JS
+
+document.write(6 + 6)                                     //example of an expression
