063f653b4c19280d4e0158c4790033aa15db7254
Stephanie <sjensen88@gmail.com>
2024-01-29 14:51:02 -0600

    fixes

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,10 @@
 function addition_function() {                                  //defines and names the function
     var str = "13!";                                            //defines a variable and gives it a string value
     document.getElementById("add").innerHTML = str;             //grabs the "add" id from the HTML file and returns the string variable
-
+}
                                                      
 function concatenate_function() {
     var sentence = "I am learning";
     sentence += " a lot from this course!";                     //+ operator concatenates the string variable
     document.getElementById("message").innerHTML = sentence;    //grabs the "message" element from the HTML file and returns the sentence variable
 }
-
