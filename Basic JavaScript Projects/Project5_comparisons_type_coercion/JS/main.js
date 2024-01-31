22a0b5bbf8b55ab0b2a8bbbe76ee82232a79d5f4
Stephanie <sjensen88@gmail.com>
2024-01-31 14:31:32 -0600

    coercion

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+document.write("10" + 5);
+    typeof(10);                        //typeof operator converts the string "10" to a number
+
+
+document.write("24" + 5);
+    typeof("The answer is ") + num;    //coercion from number to string
