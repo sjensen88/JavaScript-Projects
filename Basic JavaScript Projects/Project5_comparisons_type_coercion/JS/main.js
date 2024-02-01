ed33dc59351a6480d291f0ba2a65f83c9f96d015
Stephanie <sjensen88@gmail.com>
2024-02-01 13:16:56 -0600

    struggling with console.log syntax

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -4,3 +4,38 @@ document.write("10" + 5);
 
 document.write("24" + 5);
     typeof("The answer is ") + num;    //coercion from number to string
+
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0 / 0;
+}
+
+
+function true_Function() {
+    document.getElementById("Test2").innerHTML = isNaN("This is a string");
+}
+
+function false_Function() {
+    document.getElementById("Test3").innerHTML = isNaN("56");
+}
+
+function inf_Function() {
+    document.write(2E310);
+}
+
+function neginf_Function() {
+    document.write(-3E310);
+}
+
+function boolean_Function() {
+    document.write(10>2);
+}
+
+
+function simple_Function() {
+    let num = 16;
+    let sqrt = Math.sqrt(num);
+    console.log(sqrt);
+}
+
+
